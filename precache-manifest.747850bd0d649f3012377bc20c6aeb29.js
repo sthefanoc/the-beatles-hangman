fc0103000f1814030631b6852a8e56956f33aa8e
Sthefano Carvalho <sthefano@masteradin.com>
2020-08-13 09:18:13 -0300

    Updates

---
+++ precache-manifest.747850bd0d649f3012377bc20c6aeb29.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d632d722d78518454a1347fbe087a63",
+    "url": "/the-beatles-hangman/index.html"
+  },
+  {
+    "revision": "60d2d2adf5e92c3104ed",
+    "url": "/the-beatles-hangman/static/css/main.a029e769.chunk.css"
+  },
+  {
+    "revision": "4e85024af6c816922cb8",
+    "url": "/the-beatles-hangman/static/js/2.d85a0213.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/the-beatles-hangman/static/js/2.d85a0213.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60d2d2adf5e92c3104ed",
+    "url": "/the-beatles-hangman/static/js/main.7c6cf616.chunk.js"
+  },
+  {
+    "revision": "b7c93c9bc15068beeeb2",
+    "url": "/the-beatles-hangman/static/js/runtime-main.9e236aa2.js"
+  },
+  {
+    "revision": "6dea6c76007afef3801fe837f640fe18",
+    "url": "/the-beatles-hangman/static/media/abbey-road-bg.6dea6c76.png"
+  },
+  {
+    "revision": "d9bfd5fa777da34bcc1b2a844ffce5a8",
+    "url": "/the-beatles-hangman/static/media/abbey-road-bg.d9bfd5fa.jpg"
+  }
+]);
